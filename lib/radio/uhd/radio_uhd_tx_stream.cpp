b249c4a980324e9b64f5a69821e533d3ba7d1529
BigTy <tylerbibus@hotmail.com>
2025-03-10 13:40:35 -0500

    modified debug print

--- lib/radio/uhd/radio_uhd_tx_stream.cpp
+++ lib/radio/uhd/radio_uhd_tx_stream.cpp
@@ -35,7 +35,7 @@ void set_usrp_gpio(uhd::usrp::multi_usrp::sptr usrp, bool tx_active){
     fmt::print("USRP device not initialized\n");
     return;
   }
-  fmt::print("Switch GPIO\n");
+  fmt::print("Switch GPIO {}\n", tx_active);
   //GPIO HIGH for TX, LOW for RX/IDLE
   uint32_t gpio_value = tx_active ? 0x7f : 0x00;
   //Use FP0 (Front panel 0) confirm GPIO_Bank value
